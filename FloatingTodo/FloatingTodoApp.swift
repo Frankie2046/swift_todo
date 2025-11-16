238503d636763e4f359f2c042d99a06462552436
frank <francis-233@outlook.com>
2025-11-16 22:30:57 +0800

    Initial Commit

---
+++ FloatingTodo/FloatingTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTodoApp.swift
+//  FloatingTodo
+//
+//  Created by fynn on 2025/11/16.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
